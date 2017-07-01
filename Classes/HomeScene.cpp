aee3edb10b0dee379688585acb9cd137dc22e84c
AlbertChen1995 <524079972@qq.com>
2017-07-01 23:42:43 +0800

    Add files via upload

---
+++ Classes/HomeScene.cpp
@@ -0,0 +1,132 @@
+#include "HomeScene.h"
+#include "cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+#include "json/rapidjson.h"
+#include "json/document.h"
+#include "json/writer.h"
+#include "json/stringbuffer.h"
+#include "GameScene.h"
+#include <regex>
+using std::to_string;
+using std::regex;
+using std::match_results;
+using std::regex_match;
+using std::cmatch;
+using namespace rapidjson;
+
+
+using namespace cocostudio::timeline;
+
+#include "json/document.h"
+#include "json/writer.h"
+#include "json/stringbuffer.h"
+using namespace  rapidjson;
+
+using namespace cocostudio::timeline;
+
+Scene* HomeScene::createScene()
+{
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+
+	// 'layer' is an autorelease object
+	auto layer = HomeScene::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HomeScene::init()
+{
+	Size size = Director::getInstance()->getVisibleSize();
+	visibleHeight = size.height;
+	visibleWidth = size.width;
+
+	auto btnJoinRoom = Button::create();
+	btnJoinRoom->setTitleText("Login");
+	btnJoinRoom->setTitleFontSize(30);
+	btnJoinRoom->setPosition(Size(visibleWidth / 3, visibleHeight * 2 / 3));
+	this->addChild(btnJoinRoom, 2);
+
+	btnJoinRoom->addTouchEventListener(CC_CALLBACK_2(HomeScene::JoinRoom, this));
+
+	auto btnCreateRoom = Button::create();
+	btnCreateRoom->setTitleText("Login");
+	btnCreateRoom->setTitleFontSize(30);
+	btnCreateRoom->setPosition(Size(visibleWidth / 3, visibleHeight / 3));
+	this->addChild(btnCreateRoom, 2);
+
+	btnCreateRoom->addTouchEventListener(CC_CALLBACK_2(HomeScene::CreateRoom, this));
+	//	button->addClickEventListener(ui::Widget::ccWidgetClickCallback(LogInScene::login));
+	/**  you can create scene with following comment code instead of using csb file.
+	// 1. super init first
+	if ( !Layer::init() )
+	{
+	return false;
+	}
+
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	/////////////////////////////
+	// 2. add a menu item with "X" image, which is clicked to quit the program
+	//    you may modify it.
+
+	// add a "close" icon to exit the progress. it's an autorelease object
+	auto closeItem = MenuItemImage::create(
+	"CloseNormal.png",
+	"CloseSelected.png",
+	CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+	origin.y + closeItem->getContentSize().height/2));
+
+	// create menu, it's an autorelease object
+	auto menu = Menu::create(closeItem, NULL);
+	menu->setPosition(Vec2::ZERO);
+	this->addChild(menu, 1);
+
+	/////////////////////////////
+	// 3. add your codes below...
+
+	// add a label shows "Hello World"
+	// create and initialize a label
+
+	auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+
+	// position the label on the center of the screen
+	label->setPosition(Vec2(origin.x + visibleSize.width/2,
+	origin.y + visibleSize.height - label->getContentSize().height));
+
+	// add the label as a child to this layer
+	this->addChild(label, 1);
+
+	// add "HelloWorld" splash screen"
+	auto sprite = Sprite::create("HelloWorld.png");
+
+	// position the sprite on the center of the screen
+	sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+	// add the sprite as a child to this layer
+	this->addChild(sprite, 0);
+	**/
+
+	//////////////////////////////
+	// 1. super init first
+	if (!Layer::init())
+	{
+		return false;
+	}
+
+	//	    auto rootNode = CSLoader::createNode("MainScene.csb");
+
+	//	    addChild(rootNode);
+
+	return true;
+}
---
+++ Classes/RoomScene.h
@@ -0,0 +1,37 @@
+#ifndef __ROOM_SCENE_H__
+#define __ROOM_SCENE_H__
+
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+#include "network/HttpClient.h"
+#include <string>
+#include <vector>
+using std::vector;
+using std::string;
+using namespace cocos2d::ui;
+using namespace cocos2d::network;
+
+class RoomScene : public cocos2d::Layer
+{
+public:
+	// there's no 'id' in cpp, so we recommend returning the class instance pointer
+	static cocos2d::Scene* createScene(string userName);
+
+	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+	virtual bool init();
+
+	// implement the "static create()" method manually
+	CREATE_FUNC(RoomScene);
+
+	void RoomScene::Ready(Ref *pSender, Widget::TouchEventType type);
+	void RoomScene::Back(Ref *pSender, Widget::TouchEventType type);
+
+private:
+	float visibleHeight;
+	float visibleWidth;
+	TextField * textField;
+	TextField * textBox;
+	Button * btnReady;
+};
+
+#endif // __HELLOWORLD_SCENE_H__
